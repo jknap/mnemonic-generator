e029ed5874254db2831d9df1796d1af6586d2c74
Jean Knapik <jean.knapik@consensys.net>
2018-08-29 17:59:36 +0200

    chore(test): create tests

--- test/integration.js
+++
@@ -1,8 +0,0 @@
-const generator = require('../');
-
-const userAddress = process.argv[2];
-
-const mnemonic = generator(userAddress);
-
-console.log(`User address: ${userAddress}`);
-console.log(`Corresponding mnemonic: ${mnemonic}`);
---
+++ test/test.js
@@ -0,0 +1,13 @@
+var assert = require('assert');
+var generator = require('../');
+
+var ADDRESS = '0xdd67Ee2d65a89F827eC9c0335a93F8105Aa969b8';
+var MNEMONIC = 'everest wheel'
+
+
+describe('Generator', function() {
+  it('should return the mnemonic corresponding to the address', function() {
+      mnemonic = generator(ADDRESS)
+      assert.equal(mnemonic, MNEMONIC);
+  });
+});
