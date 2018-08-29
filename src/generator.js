f20d58c5023af75085bf654481bcce25db59029b
Jean Knapik <jean.knapik@consensys.net>
2018-08-29 17:27:29 +0200

    feat(mnemonic): add a second word through a salt to reduce collisions

--- src/generator.js
+++ src/generator.js
@@ -1,10 +1,17 @@
 const hash = require('object-hash');
 const words = require('./constants/words')
 
-const addressToMnemonic = (address) => {
-  addressHash = hash(address);
-  index = parseInt(addressHash, 16) % words.length;
+const SALT = "SCHWIFTY"
+
+const getSingleMnemonic = (address) => {
+  objHash = hash(address);
+  index = parseInt(objHash, 16) % words.length;
   return words[index];
+}
+const addressToMnemonic = (address) => {
+  word1 = getSingleMnemonic(address);
+  word2 = getSingleMnemonic(address + SALT);
+  return word1 + ' ' + word2
 }
 
 module.exports = addressToMnemonic;
